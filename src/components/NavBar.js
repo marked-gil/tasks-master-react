fd6d10e7a0f4eefc73c72f216081c0f61060ab24
marked-gil <markgil_creates@yahoo.com>
2023-03-30 01:59:45 +0000

    Use NavBar offcanvas

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,42 +1,54 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import styles from '../styles/NavBar.module.css'
 
-function NavScrollExample() {
+function OffcanvasExample() {
+
   return (
-    <Navbar expand="lg" className={styles.NavBar}>
-      <Container fluid className="d-flex">
-        <Navbar.Brand href="#" className="p-2 flex-grow-1">Logo Tasks Master</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Form className="d-flex ms-5">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-          <Nav
-            className={`ms-auto me-3 my-2 my-lg-0`}
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#">Home</Nav.Link>
-            <Nav.Link href="#">My Tasks</Nav.Link>
-            <Nav.Link href="#">Sign Out</Nav.Link>
-          </Nav>
-
-          <div>AVATAR</div>
-
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      {['lg'].map((expand) => (
+        <Navbar key={expand} expand={expand} className={`mb-3 ${styles.NavBar}`}>
+          <Container fluid>
+            <Navbar.Brand href="#" className="p-2 flex-grow-1">LOGO Tasks Master</Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Tasks Master
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Form className="d-flex">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    name="search"
+                  />
+                  <Button variant="outline-success">Search</Button>
+                </Form>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#">Home</Nav.Link>
+                  <Nav.Link href="#">My Tasks</Nav.Link>
+                  <Nav.Link href="#">Sign Out</Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
   );
 }
 
-export default NavScrollExample;
+export default OffcanvasExample;
