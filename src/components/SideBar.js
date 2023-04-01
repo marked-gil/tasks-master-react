3d8e0712160d275cc07eb457ce248b3c103cc244
marked-gil <markgil_creates@yahoo.com>
2023-04-01 20:15:57 +0000

    Adjust layout on sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -3,7 +3,27 @@ import styles from '../styles/SideBar.module.css'
 
 const SideBar = () => {
   return (
-    <div className={styles.SideBar}></div>
+    <div className={styles.SideBar}>
+      <ul className="ps-0">
+        <li><a href="#"><i class="fa-solid fa-plus"></i> Add Task</a></li>
+        <li><a href="#"><i class="fa-solid fa-calendar-week"></i> Today</a></li>
+        <li><a href="#"><i class="fa-sharp fa-solid fa-forward"></i> Tomorrow</a></li>
+        <li><a href="#"><i class="fa-solid fa-list-check"></i> All My Tasks</a></li>
+        <li><a href="#"><i class="fa-sharp fa-solid fa-share"></i> Shared Tasks</a></li>
+        <li><a href="#"><i class="fa-sharp fa-solid fa-bell"></i> Overdue Tasks</a></li>
+      </ul>
+      <div>
+        <div className="d-flex align-items-center">
+          <h2 className="me-3">Categories</h2>
+          <a href="#"><i class="fa-solid fa-plus"></i> Add New</a>
+        </div>
+        <ul className="ps-0">
+          <li><a href="#">At Home</a></li>
+          <li><a href="#">At Work</a></li>
+        </ul>
+      </div>
+      <a href="#"><i class="fa-solid fa-check-double"></i> Recently Completed Tasks</a>
+    </div>
   )
 }
 
