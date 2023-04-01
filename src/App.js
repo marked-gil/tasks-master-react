5c61dda81a3c4305c7c7af577d0375ebeae198ea
marked-gil <markgil_creates@yahoo.com>
2023-04-01 04:29:07 +0000

    Create initial Sign Up page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import styles from './App.module.css';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar'
+import SignUpPage from './pages/auth/SignUpPage';
 import MainContent from './pages/MainContent';
 
 function App() {
   return (
     <div className={styles.App}>
-      <NavBar />
+      <SignUpPage />
+      {/* <NavBar />
       <MainContent />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
---
+++ src/pages/auth/SignUpPage.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import styles from '../../styles/SignUpPage.module.css'
+
+function SignUpPage() {
+  return (
+    <div className={styles.SignUp}>
+      <div className={styles.SignUpBox}>
+        <p className="mb-5">LOGO - Tasks Master</p>
+        <h1>Sign Up</h1>
+        <Form className={`mt-5 ${styles.Form}`}>
+          <Form.Group className="mb-3" controlId="username">
+            <Form.Label>Username:</Form.Label>
+            <Form.Control type="text" placeholder="Enter a username" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Enter a password" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="repeat-password">
+            <Form.Label>Password (again)</Form.Label>
+            <Form.Control type="password" placeholder="Repeat the password" />
+            <Form.Text className="text-muted">
+              
+              </Form.Text>
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Register
+          </Button>
+        </Form>
+        <div className="d-flex mt-5">
+          <p className="me-5">Already registered?</p>
+          <a href="#">SIGN IN</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignUpPage;
