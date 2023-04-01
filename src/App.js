08ade8e76afbe4c8d44c24642cd6937fea25334e
marked-gil <markgil_creates@yahoo.com>
2023-04-01 14:57:59 +0000

    Add routes to created pages

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Route, Switch } from 'react-router-dom';
 import styles from './App.module.css';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar'
@@ -8,11 +9,12 @@ import MainContent from './pages/MainContent';
 function App() {
   return (
     <div className={styles.App}>
-      {/* <SignInPage /> */}
-      <SignUpPage />
-      {/* <NavBar />
-      <MainContent />
-      <Footer /> */}
+      <Switch>
+        <Route exact path="/" render={() => <SignInPage /> } />
+        <Route exact path="/signup" render={() => <SignUpPage /> } />
+        <Route exact path="/profile" render={() => <MainContent /> } />
+        <Route render={() => <h1>Page Not Found</h1>}/>
+      </Switch>
     </div>
   );
 }
--- src/pages/MainContent.js
+++ src/pages/MainContent.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
+import Footer from '../components/Footer'
+import NavBar from '../components/NavBar'
 import SideBar from '../components/SideBar'
 import styles from '../styles/MainContent.module.css'
 import ProfilePage from './profiles/ProfilePage'
 
 function MainContent() {
   return (
-    <Container fluid className={styles.MainContent}>
-      <Row className={styles.Row}>
-        <SideBar />
-        <ProfilePage />
-        {/* <Col></Col> */}
-      </Row>
-    </Container>
+    <>
+      <NavBar />
+      <Container fluid className={styles.MainContent}>
+        <Row className={styles.Row}>
+          <SideBar />
+          <ProfilePage />
+        </Row>
+      </Container>
+      <Footer />
+    </>
+    
   )
 }
 
