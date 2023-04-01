449f187be4e7627e10338ad335cc3221d1524a69
marked-gil <markgil_creates@yahoo.com>
2023-04-01 04:44:15 +0000

    Create initial Sign In page layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import styles from './App.module.css';
 import Footer from './components/Footer';
 import NavBar from './components/NavBar'
+import SignInPage from './pages/auth/SignInPage';
 import SignUpPage from './pages/auth/SignUpPage';
 import MainContent from './pages/MainContent';
 
 function App() {
   return (
     <div className={styles.App}>
-      <SignUpPage />
-      {/* <NavBar />
+      <SignInPage />
+      {/* <SignUpPage />
+      <NavBar />
       <MainContent />
       <Footer /> */}
     </div>
---
+++ src/pages/auth/SignInPage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import styles from '../../styles/SignInPage.module.css'
+
+function SignInPage() {
+  return (
+    <div className={styles.SignInPage}>
+      <div className={styles.SignInBox}>
+        <p className="mb-5">LOGO - Tasks Master</p>
+        <h1>Sign In</h1>
+        <Form className={`mt-5 ${styles.Form}`}>
+          <Form.Group className="mb-3" controlId="username">
+            <Form.Label className={styles.bold}>Username:</Form.Label>
+            <Form.Control type="text" placeholder="Enter your username" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="password">
+            <Form.Label className={styles.bold}>Password:</Form.Label>
+            <Form.Control type="password" placeholder="Enter your password" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Register
+          </Button>
+        </Form>
+        
+        <div className="d-flex flex-column mt-5">
+          <a className="mb-4" href="#">Forgot Password?</a>
+          <p>Not registered? <a className="ms-4" href="#">SIGN UP</a></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SignInPage;
