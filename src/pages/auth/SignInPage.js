29f626447ee81ee498d0711873e997ffa98b0cc0
marked-gil <markgil_creates@yahoo.com>
2023-04-02 16:00:49 +0000

    Connect Sign In to API

--- src/pages/auth/SignInPage.js
+++ src/pages/auth/SignInPage.js
@@ -1,24 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import styles from '../../styles/SignInPage.module.css'
 
 function SignInPage() {
+
+  const [signInData, setSignInData] = useState({
+    username: "",
+    password: ""
+  });
+
+  const { username, password } = signInData;
+  const { errors, setErrors } = useState({});
+
+  const history = useHistory();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const { data } = await axios.post("/dj-rest-auth/login/", signInData);
+      history.push('/profile');
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
     <div className={styles.SignInPage}>
       <div className={styles.SignInBox}>
         <p className="mb-5">LOGO - Tasks Master</p>
         <h1>Sign In</h1>
-        <Form className={`d-flex flex-column mt-5 ${styles.Form}`}>
+        <Form onSubmit={handleSubmit} className={`d-flex flex-column mt-5 ${styles.Form}`}>
           <Form.Group className="mb-3" controlId="username">
             <Form.Label className={styles.bold}>Username:</Form.Label>
-            <Form.Control type="text" placeholder="Enter your username" />
+            <Form.Control
+              type="text"
+              placeholder="Enter your username"
+              name="username"
+              value={username}
+              onChange={handleChange}
+            />
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="password">
             <Form.Label className={styles.bold}>Password:</Form.Label>
-            <Form.Control type="password" placeholder="Enter your password" />
+            <Form.Control
+              type="password"
+              placeholder="Enter your password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+            />
           </Form.Group>
 
           <Button variant="primary" type="submit">
