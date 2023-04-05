1e154b8b319b8270cfb4d764fc285713682a1fbd
marked-gil <markgil_creates@yahoo.com>
2023-04-05 20:35:12 +0000

    Use Link from React-router lib

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useCurrentUser } from '../contexts/CurrentUserContext'
 import styles from '../styles/SideBar.module.css'
+import { Link } from 'react-router-dom';
 
 const SideBar = () => {
   const currentUser = useCurrentUser();
@@ -9,24 +10,40 @@ const SideBar = () => {
     <div className={styles.SideBar}>
       <p>Hi, {currentUser?.username}!</p>
       <ul className="ps-0 mb-5">
-        <li className="mb-2"><a href="#"><i className="fa-solid fa-plus"></i> Add Task</a></li>
-        <li className="mb-2"><a href="#"><i className="fa-solid fa-calendar-week"></i> Today</a></li>
-        <li className="mb-2"><a href="#"><i className="fa-sharp fa-solid fa-forward"></i> Tomorrow</a></li>
-        <li className="mb-2"><a href="#"><i className="fa-solid fa-list-check"></i> All My Tasks</a></li>
-        <li className="mb-2"><a href="#"><i className="fa-sharp fa-solid fa-share"></i> Shared Tasks</a></li>
-        <li className="mb-2"><a href="#"><i className="fa-sharp fa-solid fa-bell"></i> Overdue Tasks</a></li>
+        <li className="mb-2">
+          <Link><i className="fa-solid fa-plus"></i> Add Task</Link>
+        </li>
+        <li className="mb-2">
+          <Link to="/"><i className="fa-solid fa-calendar-week"></i> Today</Link>
+        </li>
+        <li className="mb-2">
+          <Link><i className="fa-sharp fa-solid fa-forward"></i> Tomorrow</Link>
+        </li>
+        <li className="mb-2">
+          <Link><i className="fa-solid fa-list-check"></i> All My Tasks</Link>
+        </li>
+        <li className="mb-2">
+          <Link><i className="fa-sharp fa-solid fa-share"></i> Shared Tasks</Link>
+        </li>
+        <li className="mb-2">
+          <Link><i className="fa-sharp fa-solid fa-bell"></i> Overdue Tasks</Link>
+        </li>
       </ul>
       <div>
         <div className="d-flex align-items-center">
           <h2 className="me-3">Categories</h2>
-          <a href="#"><i className="fa-solid fa-plus"></i> Add New</a>
+          <Link><i className="fa-solid fa-plus"></i> Add New</Link>
         </div>
         <ul className="ps-0">
-          <li className="mb-2"><a href="#">At Home</a></li>
-          <li className="mb-2"><a href="#">At Work</a></li>
+          <li className="mb-2">
+            <Link>At Home</Link>
+          </li>
+          <li className="mb-2">
+            <Link>At Work</Link>
+          </li>
         </ul>
       </div>
-      <a href="#"><i className="fa-solid fa-check-double"></i> Recently Completed Tasks</a>
+      <Link><i className="fa-solid fa-check-double"></i> Recently Completed Tasks</Link>
     </div>
   )
 }
