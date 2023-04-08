e583d4c1dc6d6a7b4f85431c948c5df294cbc004
marked-gil <markgil_creates@yahoo.com>
2023-04-08 01:30:54 +0000

    Create css module for AllTasksPage

--- src/pages/tasks/AllTasksPage.js
+++ src/pages/tasks/AllTasksPage.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import moment from 'moment';
+import { Col } from 'react-bootstrap';
+import ErrorDisplay from '../../components/ErrorDisplay';
+import styles from '../../styles/AllTasksPage.module.css';
+
 
 function AllTasksPage() {
+
+  const [ error, setError ] = useState({});
+
+
   return (
-    <div>AllTasksPage</div>
+    <Col className={styles.AllTasks}>
+      <div className={styles.InnerContainer}>
+        {error?.data && <ErrorDisplay error={error} />}
+
+        <div className={`d-flex justify-content-between`}>
+          <h2 className={`${styles.Heading}`}>My Tasks</h2>
+          <span className={styles.LineIcon}><i className="fa-solid fa-ellipsis-vertical"></i></span> 
+          <h2 className={`${styles.Heading}`}>ALL</h2>
+        </div>
+
+        <hr />
+      </div>
+    </Col>
   )
 };
 
