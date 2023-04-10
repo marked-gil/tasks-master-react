1931e3ec99e3c2e4e0e9cc6c0b1b9569e5942553
marked-gil <markgil_creates@yahoo.com>
2023-04-10 20:08:18 +0000

    Set up display layout of Task Details page

--- src/pages/tasks/TaskDetailsPage.js
+++ src/pages/tasks/TaskDetailsPage.js
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import styles from '../../styles/TaskDetailsPage.module.css';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { axiosReq } from '../../api/axiosDefaults';
 import { useParams } from 'react-router-dom';
+import { FloatingLabel } from 'react-bootstrap';
+import { getCategories } from '../../api/categoryMethods';
 
 function TaskDetailsPage() {
 
-  const [ taskData, setTaskData ] = useState({})
+  const { id } = useParams();
+  const [ categories, setCategories ] = useState({ results: []});
+  const [ taskData, setTaskData ] = useState({});
 
   const {
     task_name, 
@@ -20,54 +24,61 @@ function TaskDetailsPage() {
     progress
   } = taskData;
 
-  const { id } = useParams();
-
   useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(`/tasks/${id}`);
-        setTaskData(data)
-        console.log(data)
-      } catch (err) {
-        console.log(err.response?.data)
-      }
-    };
+    getCategories(setCategories);
+  }, []);
 
-    handleMount();
-  }, [id] )
+ useMemo(() => {
+    const handleMount = async () => {
+          try {
+            const { data } = await axiosReq.get(`/tasks/${id}`);
+            setTaskData(data)
+            console.log("this is the data returned:", data)
+          } catch (err) {
+            console.log(err.response?.data)
+          }
+        };
+    
+        handleMount();
+  }, [id])
 
   const handleChange = (event) => {
-    setTaskData({
-      ...taskData,
-      [event.target.name]: event.target.value
-    })
+    setTaskData(prevState => (
+      {
+        ...prevState,
+        [event.target.name]: event.target.value
+      }
+    ))
   }
 
   return (
     <Col className={styles.TaskDetails}>
-      <div className={styles.Container}>
+      <div className={`${styles.Container} position-relative`}>
         <h2 className={`${styles.MyTasks}`}>Task Details</h2>
-        <p>Category: {category}</p>
-        <p>
-          <span>Due: {due_date} {due_time ? `- ${due_time}` : ""}</span> | 
-          <span>{priority === 1 ? "Low" : priority === 2 ? "Medium" : "High"}</span> | 
-          <span>{progress}</span>
-        </p>  
-
-        <Form.Group className="mb-3" controlId="taskName">
-          <Form.Label>Task Name</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Task Name"
-            readOnly
-            maxLength={50}
-            className={styles.TaskName}
-            name="task_name"
-            defaultValue={task_name}
-            onChange={handleChange}
-            aria-label="Add the task's name"
-          />
-          <Button size="sm">edit</Button>
+        <div className="d-flex flex-column mb-2">
+          <p className="mb-0">Category: {category} <a href="">edit</a></p>
+          <p className="mb-0">
+            <span>Due: {due_date} {due_time ? `- ${due_time}` : ""}</span> | 
+            <span>{priority === 1 ? "Low" : priority === 2 ? "Medium" : "High"}</span> | 
+            <span>{progress}</span> <a href="">edit</a>
+          </p>  
+        </div>
+        <Button size="sm" variant='danger' className={styles.DeleteButton}>Delete Task</Button>
+        <Form.Group className="mb-3 position-relative" controlId="taskName">
+          <FloatingLabel controlId="floatingTaskNameArea" label="Task Name">
+            <Form.Control
+              type="text"
+              // placeholder="Task Name"
+              readOnly
+              maxLength={50}
+              className={styles.TaskName}
+              name="task_name"
+              defaultValue={task_name}
+              onChange={handleChange}
+              aria-label="Add the task's name"
+            />
+          </FloatingLabel>
+          <Button size="sm" className={`position-absolute bottom-0 end-0`}>edit</Button>
 
           {/* {errors.task_name?.map((error, idx) => (
             <Alert className={`mt-1 mb-0 pb-0 pt-0 ${styles.TextCenter}`} key={idx} variant="danger">
@@ -80,20 +91,21 @@ function TaskDetailsPage() {
           } */}
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="taskDescription">
-          <Form.Label>Description</Form.Label>
-          <Form.Control
-            as="textarea" 
-            rows={3} 
-            name="details"
-            readOnly
-            defaultValue={details}
-            placeholder="Description" 
-            maxLength={250}
-            onChange={handleChange}
-            aria-label="Add the task's description or details"
-          />
-          <Button size="sm">edit</Button>
+        <Form.Group className="mb-3 position-relative" controlId="taskDescription">
+          <FloatingLabel controlId="floatingTextarea" label="Description">
+            <Form.Control
+              as="textarea" 
+              style={{height: '150px'}}
+              name="details"
+              readOnly
+              defaultValue={details}
+              maxLength={250}
+              onChange={handleChange}
+              aria-label="Add the task's description or details"
+            />
+          </FloatingLabel>
+          
+          <Button size="sm" className={`position-absolute bottom-0 end-0`}>edit</Button>
           {/* {errors.details?.map((error, idx) => (
             <Alert className={`mt-1 mb-0 pb-0 pt-0 ${styles.TextCenter}`} key={idx} variant="danger">
               {error}
@@ -101,8 +113,15 @@ function TaskDetailsPage() {
             ))
           } */}
         </Form.Group>
+        
+        {/* COMMENT SECTION */}
+        <div>
+          <a href="">Add comment</a>
+          <div>
+            <h2>Comments</h2>
+          </div>
 
-
+        </div>
       </div>
     </Col>
   )
