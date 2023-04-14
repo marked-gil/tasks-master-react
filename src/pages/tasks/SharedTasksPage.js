e8cf076124d59f002831e9b49fa61ad9a333846e
marked-gil <markgil_creates@yahoo.com>
2023-04-14 14:33:23 +0000

    Retrieve data for SharedTasksPage

--- src/pages/tasks/SharedTasksPage.js
+++ src/pages/tasks/SharedTasksPage.js
@@ -1,8 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Col } from 'react-bootstrap';
+import ErrorDisplay from '../../components/ErrorDisplay';
+import TasksFilter from '../../components/TasksFilter';
+import TasksList from './TasksList';
+import styles from '../../styles/SharedTasksPage.module.css';
+import { getFilteredTasks } from '../../api/taskMethods';
+import { axiosReq } from '../../api/axiosDefaults';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
 
 function SharedTasksPage({ categories }) {
+  const currentUser = useCurrentUser();
+  const id = currentUser?.pk
+
+  const [ tasks, setTasks ] = useState({ results: []});
+  const [ filters, setFilters ] = useState({category_name: "", progress: "", order_by: ""});
+  const [ error, setError ] = useState({});
+
+  useEffect(() => {
+    const getSharedTasks = async () => {
+      try {
+        const { data } = await axiosReq.get(`/tasks/?shared_to=${id}`);
+        setTasks(data);
+        console.log("shared", data)
+      } catch (err) {
+        console.log(err.response?.data);
+        setError(err.response)
+      }
+    }
+    if (id) {
+      getSharedTasks();
+    }
+  }, [id]);
+
   return (
-    <div>SharedTasksPage</div>
+    <Col className={styles.SharedTasksPage}>
+      <div className={styles.InnerContainer}>
+        {error?.data && <ErrorDisplay error={error} />}
+
+        <div className={`d-flex justify-content-between`}>
+          <h2 className={`${styles.Heading}`}>My Tasks</h2>
+          <span className={styles.LineIcon}><i className="fa-solid fa-ellipsis-vertical"></i></span> 
+          <h2 className={`${styles.Heading}`}>SHARED</h2>
+        </div>
+
+        <TasksFilter 
+          setFilters={setFilters}
+          categories={categories}
+          // handleFilterSubmit={handleFilterSubmit}
+          removeOrderByTime
+          removeProgressField
+        />
+
+        <hr />
+      
+        <TasksList
+          tasks={tasks}
+          setTasks={setTasks}
+          showCompletedTasks={true}
+          showDate
+          removeDoneButton
+        />
+      </div>
+
+    </Col>
   )
 };
 
