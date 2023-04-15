4dc6ded20ec517d9c9713bdf3b130a4d21d85660
marked-gil <markgil_creates@yahoo.com>
2023-04-15 22:35:40 +0000

    Add feedback message for image validation errors

--- src/pages/profiles/UpdateProfileImage.js
+++ src/pages/profiles/UpdateProfileImage.js
@@ -5,10 +5,16 @@ import { axiosReq } from '../../api/axiosDefaults';
 function UpdateProfileImage({ profile_id, profileData, setProfileData, className }) {
   
   const [ imageSource, setImageSource ] = useState("");
+  const [ errors, setErrors ] = useState({})
   const [show, setShow] = useState(false);
   const imageFile = useRef();
 
-  const handleClose = () => setShow(false);
+  const handleClose = () => {
+    setShow(false)
+    setImageSource(profileData.image)
+    setErrors({})
+  };
+
   const handleShow = () => setShow(true);
 
   useEffect(() => {
@@ -22,15 +28,15 @@ function UpdateProfileImage({ profile_id, profileData, setProfileData, className
   const handleSubmit = async () => {
     const formData = new FormData();
     formData.append('image', imageFile?.current?.files[0]);
-
-    try {
-      console.log(formData)
-      const { data } = await axiosReq.put(`profiles/${profile_id}/`, formData);
-      setProfileData(data);
-      URL.revokeObjectURL(imageSource);
-      handleClose();
-    } catch (err) {
-      console.log(err.response?.data)
+    if (imageFile?.current?.files[0]) {
+      try {
+        const { data } = await axiosReq.put(`profiles/${profile_id}/`, formData);
+        setProfileData(data);
+        handleClose();
+        URL.revokeObjectURL(imageSource);
+      } catch (err) {
+        setErrors(err.response?.data)
+      }
     }
   };
 
@@ -44,7 +50,7 @@ function UpdateProfileImage({ profile_id, profileData, setProfileData, className
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Upload Profile Image</Modal.Title>
+          <Modal.Title>Update Profile Image</Modal.Title>
         </Modal.Header>
         <Modal.Body className="d-flex flex-column align-items-center">
         <Figure>
@@ -58,12 +64,14 @@ function UpdateProfileImage({ profile_id, profileData, setProfileData, className
         <Form.Group controlId="formFile" className="mb-3">
           <Form.Control
             ref={imageFile}
-            // name="image"
             type="file"
             accept="image/*"
             onChange={handleChangeFile}
           />
         </Form.Group>
+        {errors?.image?.map((err_message, idx) => (
+          <p className="mb-0" style={{ color:"red" }} key={idx}>{err_message}</p>
+        ))}
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose} size="sm">
