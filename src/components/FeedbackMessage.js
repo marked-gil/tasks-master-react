b01c7d2965ce96d4b31c25b2a0c39f0f8ea276e1
marked-gil <markgil_creates@yahoo.com>
2023-04-19 21:22:20 +0000

    Give FeedbackMessage component variant options- success vs danger

--- src/components/FeedbackMessage.js
+++ src/components/FeedbackMessage.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 
-function FeedbackMessage({ message }) {
+function FeedbackMessage({ message, danger }) {
   const [show, setShow] = useState(true);
+
+  const variant = danger ? 'danger' : 'success'
 
   if (show) {
     return (
       <Alert
-        variant="success"
+        variant={variant}
         onClose={() => setShow(false)} 
         dismissible
         style={{ marginTop:"-35px"}}
