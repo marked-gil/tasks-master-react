d58c74b909bad8dfab3a07f8103634a235341943
marked-gil <markgil_creates@yahoo.com>
2023-04-20 15:06:57 +0000

    Add styling to SearchCard

--- src/pages/tasks/SearchResultsPage.js
+++ src/pages/tasks/SearchResultsPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Col } from 'react-bootstrap';
 import styles from '../../styles/SearchResultsPage.module.css';
+import SearchCard from '../../components/SearchCard';
 
 function SearchResultsPage() {
   return (
     <Col className={styles.SearchResultsPage}>
       <div className={styles.InnerContainer}>
         <h2>Search Results:</h2>
-        
+        <SearchCard TitleStyle={styles.TaskTitle} TextStyle={styles.TaskDescription} />
       </div>
     </Col>
   )
