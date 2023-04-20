caaae7d03d18c5f3b60591c6abf81859b40f0f7d
marked-gil <markgil_creates@yahoo.com>
2023-04-20 16:13:03 +0000

    Make the task title as link to taskdetails page

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -1,15 +1,18 @@
 import moment from 'moment';
 import React from 'react';
 import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function SearchCard({ task, TitleStyle, TextStyle }) {
   return (
     <Card className="mb-2">
       <Card.Body>
-        <Card.Title className={TitleStyle}>{task.task_name}</Card.Title>
+        <Card.Title className={TitleStyle}>
+          <Link to={`task/${task.id}`}>{task.task_name}</Link>
+        </Card.Title>
         <Card.Text className={TextStyle}>{task.details}</Card.Text>
       </Card.Body>
-      <Card.Footer className="d-flex gap-3">
+      <Card.Footer className="d-flex gap-3 pt-1 pb-1">
           <p className="mb-0"><i className="fa-solid fa-at fa-sm"></i>{task.owner}</p>
           <p className="mb-0">category: <span style={{ textTransform:"uppercase" }}>{task.category}</span></p>
           {!!task.shared_to.length && <p className="mb-0"><i class="fa-solid fa-link fa-sm"></i>Shared</p>}
