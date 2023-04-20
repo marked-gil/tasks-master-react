e9862f378125cbd5f75d8225e260138fe567d108
marked-gil <markgil_creates@yahoo.com>
2023-04-20 09:26:20 +0000

    Add edit and delete buttons on each comment

--- src/pages/tasks/TaskDetailsPage.js
+++ src/pages/tasks/TaskDetailsPage.js
@@ -247,7 +247,11 @@ function TaskDetailsPage({ categories }) {
               <Card key={comment.id} className="mb-1">
                 <Card.Title>{comment.owner}</Card.Title>
                 <Card.Subtitle>{comment.datetime_updated}</Card.Subtitle>
-                <Card.Body>{comment.content}</Card.Body>
+                <Card.Body className="pt-1 pb-1">{comment.content}</Card.Body>
+                <div className="d-flex justify-content-end gap-2 mb-1">
+                  <Button size="sm" className="pt-0 pb-0 ps-2 pe-2">Edit</Button>
+                  <Button variant="danger" size="sm" className="pt-0 pb-0 ps-2 pe-2">Delete</Button>
+                </div>
               </Card>
             ))
           }
