5023550e0870f5dcb7fbac0853208ce7b8483d3b
marked-gil <markgil_creates@yahoo.com>
2023-04-21 17:37:54 +0000

    Make Profile page responsive

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,9 +1,8 @@
 import React, { useMemo, useState } from 'react';
-import { Row, Col, Alert } from 'react-bootstrap';
+import { Col, Alert } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { axiosReq } from '../../api/axiosDefaults';
-
 import styles from '../../styles/ProfilePage.module.css';
 import UpdateProfileImage from './UpdateProfileImage';
 import LoadingIcon from '../../components/LoadingIcon';
@@ -78,14 +77,14 @@ function ProfilePage ({ currentUser }) {
 
   return (
     <Col className={styles.ProfilePage}>
-      <div className="position-relative">
+      <div className={`position-relative`}>
         {!isLoaded && <LoadingIcon size="8" />}
         {feedbackMessage && <FeedbackMessage message={feedbackMessage} />}
 
         <h2 className={styles.PageTitle}>My Profile</h2>
 
           <Form onSubmit={handleSubmit} className={styles.Form}>
-            <div className="d-flex position-relative mb-5">
+            <div className={`position-relative ${styles.ImageUsernameDiv}`}>
               <img src={image} alt="profile" className={styles.profileImage} />
               <UpdateProfileImage
                 profile_id={profile_id}
@@ -97,18 +96,18 @@ function ProfilePage ({ currentUser }) {
 
               {/* USERNAME */}
               <Form.Group 
-                className="d-flex flex-column justify-content-center ms-3 mb-3" 
+                className={`${styles.UsernameFormGroup} `}
                 controlId="username"
               >
-                <div className="d-flex">
+                <div className={`${styles.UsernameContainer}`}>
                   <Form.Label sm="2" className={`ps-0 mb-0 me-3 ${styles.bold}`}>
                     Username:
                   </Form.Label>
                   <Form.Control
+                    className={styles.UsernameFormControl}
                     type="text"
                     readOnly
                     value={owner}
-                    style={{ minWidth:"18rem"}}
                   />
                 </div>
                 <Form.Text className={`text-muted position-absolute ${styles.smallInfo}`}>
@@ -118,17 +117,17 @@ function ProfilePage ({ currentUser }) {
             </div>
 
             {/* FIRST NAME */}
-            <Form.Group as={Row} className="mb-3" controlId="first-name">
-              <Form.Label column sm="2" className={`ps-0 ${styles.bold}`}>First Name:</Form.Label>
-              <Col sm="10" className="p-0">
-                <Form.Control
-                  type="text"
-                  placeholder="Enter your first name"
-                  name="first_name"
-                  value={first_name}
-                  onChange={handleChange}
-                />
-              </Col>
+            <Form.Group className={`${styles.FirstNameGroup}`} controlId="first-name">
+              <Form.Label className={`ps-0 ${styles.bold}`} style={{ width:"8rem" }}>
+                First Name:
+              </Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your first name"
+                name="first_name"
+                value={first_name}
+                onChange={handleChange}
+              />
 
               {errors.first_name?.map((error, idx) => (
                 <Alert className={`mt-1 mb-0 pb-0 pt-0 ${styles.TextCenter}`} key={idx} variant="danger">
@@ -139,17 +138,15 @@ function ProfilePage ({ currentUser }) {
             </Form.Group>
 
             {/* LAST NAME */}
-            <Form.Group as={Row} className="mb-3" controlId="last-name">
-              <Form.Label column sm="2" className={`ps-0 ${styles.bold}`}>Last Name:</Form.Label>
-              <Col sm="10" className="p-0">
-                <Form.Control
-                  type="text"
-                  placeholder="Enter your last name"
-                  name="last_name"
-                  value={last_name}
-                  onChange={handleChange}
-                />
-              </Col>
+            <Form.Group className={`${styles.LastNameGroup}`} controlId="last-name">
+              <Form.Label className={`ps-0 ${styles.bold}`} style={{ width:"8rem" }}>Last Name:</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter your last name"
+                name="last_name"
+                value={last_name}
+                onChange={handleChange}
+              />
 
               {errors.last_name?.map((error, idx) => (
                 <Alert className={`mt-1 mb-0 pb-0 pt-0 ${styles.TextCenter}`} key={idx} variant="danger">
@@ -160,17 +157,15 @@ function ProfilePage ({ currentUser }) {
             </Form.Group>
             
             {/* EMAIL */}
-            <Form.Group as={Row} className="mb-3" controlId="email">
-              <Form.Label column sm="2" className={`ps-0 ${styles.bold}`}>Email:</Form.Label>
-              <Col sm="10" className="p-0">
-                <Form.Control
-                  type="email"
-                  placeholder="Enter your email address"
-                  name="email"
-                  value={email}
-                  onChange={handleChange}
-                />
-              </Col>
+            <Form.Group className={`${styles.EmailGroup}`} controlId="email">
+              <Form.Label className={`ps-0 ${styles.bold}`} style={{ width:"8rem" }}>Email:</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter your email address"
+                name="email"
+                value={email}
+                onChange={handleChange}
+              />
               {errors.email?.map((error, idx) => (
                 <Alert className={`mt-1 mb-0 pb-0 pt-0 ${styles.TextCenter}`} key={idx} variant="danger">
                   {error}
@@ -179,11 +174,9 @@ function ProfilePage ({ currentUser }) {
               }
             </Form.Group>
 
-            <div className='d-flex justify-content-center ps-5 pt-4'>
-              <Button variant='link' className='me-5 align-self-end'>cancel</Button>
-              <Button variant="primary" type="submit">
-                Update
-              </Button>
+            <div className={`${styles.ButtonsGroup}`}>
+              <Button variant="secondary" size="sm">cancel</Button>
+              <Button variant="primary" type="submit">Update</Button>
             </div>
           </Form>
         <div className="d-flex flex-column">
