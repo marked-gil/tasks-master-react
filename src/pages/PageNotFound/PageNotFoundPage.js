7c202f8c05bd2e6dd5d13005d9743bd31b0c548f
marked-gil <markgil_creates@yahoo.com>
2023-04-25 09:17:24 +0000

    Create 404 Page

--- src/pages/PageNotFound/PageNotFoundPage.js
+++ src/pages/PageNotFound/PageNotFoundPage.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Col } from 'react-bootstrap';
+import Col from 'react-bootstrap/Col';
+import styles from '../../styles/PageNotFoundPage.module.css';
+import icon from '../../assets/404-error.png';
+
 
 function PageNotFoundPage() {
   return (
-    <Col>
-      Page Not Found
+    <Col className={styles.PageNotFoundPage}>
+      <h2 className={styles.HeadingPageNotFound}>Page Not Found</h2>
+      <img src={icon} alt="Page Not Found Icon" className={styles.Icon}/>
     </Col>
   )
 };
