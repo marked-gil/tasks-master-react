ba87d4ef794cdf1cad3cebf35e35a88dbfec4cdc
marked-gil <markgil_creates@yahoo.com>
2023-04-25 17:05:55 +0000

    Make Footer responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,31 +4,57 @@ import styles from '../styles/Footer.module.css'
 function Footer() {
   return (
     <footer className={styles.Footer}>
-      <p className="mb-0">For Educational Purposes Only.</p>
-      <p className="mb-0"><i className="fa-sharp fa-regular fa-copyright"></i> 2023 - Tasks Master</p>          
-      <ul className="d-none d-lg-flex justify-content-between align-items-center ps-0 mb-0">
+      <p className={styles.ForEducPurposes}>For Educational Purposes Only.</p>
+      <p className={styles.CopyRight}><i className="fa-sharp fa-regular fa-copyright"></i> 2023 - Tasks Master</p>
+                
+      <ul className={styles.ListOfIcons}>
         <li>
-          <a href="https://www.facebook.com/" className={styles.icons} target="_blank" rel="noreferrer" aria-label="Open Facebook page in a new tab">
+          <a href="https://www.facebook.com/" 
+            className={styles.icon} 
+            target="_blank" 
+            rel="noreferrer" 
+            aria-label="Open Facebook page in a new tab"
+          >
             <i className="fa-brands fa-square-facebook"></i>
           </a>
         </li>
         <li>
-          <a href="https://twitter.com/" className={styles.icons} target="_blank" rel="noreferrer" aria-label="Open Twitter page in a new tab">
+          <a href="https://twitter.com/" 
+            className={styles.icon} 
+            target="_blank" 
+            rel="noreferrer" 
+            aria-label="Open Twitter page in a new tab"
+          >
             <i className="fa-brands fa-square-twitter"></i>
           </a>
         </li>
         <li>
-          <a href="https://www.youtube.com/" className={styles.icons} target="_blank" rel="noreferrer" aria-label="Open Youtube channel in a new tab">
+          <a href="https://www.youtube.com/" 
+            className={styles.icon} 
+            target="_blank" 
+            rel="noreferrer" 
+            aria-label="Open Youtube channel in a new tab"
+          >
             <i className="fa-brands fa-square-youtube"></i>
           </a>
         </li>
         <li>
-          <a href="https://www.instagram.com/" className={styles.icons} target="_blank" rel="noreferrer" aria-label="Open Instagram page in a new tab">
+          <a href="https://www.instagram.com/" 
+            className={styles.icon} 
+            target="_blank" 
+            rel="noreferrer" 
+            aria-label="Open Instagram page in a new tab"
+          >
             <i className="fa-brands fa-square-instagram"></i>
           </a>
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/mark-gil-dacutan-65863432/" className={styles.icons} target="_blank" rel="noreferrer" aria-label="Open LinkedIn page in a new tab">
+          <a href="https://www.linkedin.com/in/mark-gil-dacutan-65863432/" 
+            className={styles.icon} 
+            target="_blank" 
+            rel="noreferrer" 
+            aria-label="Open LinkedIn page in a new tab"
+          >
             <i className="fa-brands fa-linkedin"></i>
           </a>
         </li>
