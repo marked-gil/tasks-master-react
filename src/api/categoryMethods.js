835b131cac7329e3dabc563add157016429ade60
marked-gil <markgil_creates@yahoo.com>
2023-04-28 02:36:38 +0000

    Remove commented out code

--- src/api/categoryMethods.js
+++ src/api/categoryMethods.js
@@ -1,14 +1,5 @@
 import { axiosReq } from "./axiosDefaults";
 
-// export const getCategories = async (setter) => {
-//   try {
-//     const { data } = await axiosReq.get(`/categories/`);
-//     setter(data)
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
 export const getCategory = async (categoryID, setCategoryData, setError) => {
   try {
     const { data } = await axiosReq.get(`/categories/${categoryID}`);
